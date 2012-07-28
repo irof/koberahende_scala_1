65ffc90e2b0c7ee1de90b7874b73873cae5d6543
irof <irof@javaring.info>
2012-07-28 17:00:26 +0900

    fix

--- src/main/scala/scala/FizzBuzz.scala
+++ src/main/scala/scala/FizzBuzz.scala
@@ -5,8 +5,8 @@ class FizzBuzz extends interfaces.FizzBuzz {
   def invoke(num: Int): String = {
     type FB = PartialFunction[Int, String]
     val fb: FB = { case i if i % 15 == 0 => "FizzBuzz" }
-    val b: FB = { case i if i % 5 == 0 => "Fizz" }
-    val f: FB = { case i if i % 3 == 0 => "Buzz" }
+    val b: FB = { case i if i % 5 == 0 => "Buzz" }
+    val f: FB = { case i if i % 3 == 0 => "Fizz" }
     val a: FB = { case i => i.toString }
     val fizzBuzz = fb orElse f orElse b orElse a
     fizzBuzz(num)
