13c2c91895e23832b0a15da4fe180b0541020658
Taichi Uragami <backpaper0@gmail.com>
2012-07-28 16:44:19 +0900

    PartialFunctionで

--- src/main/scala/scala/FizzBuzz.scala
+++ src/main/scala/scala/FizzBuzz.scala
@@ -1,13 +1,16 @@
 package scala
 
 class FizzBuzz extends interfaces.FizzBuzz {
+
   def invoke(num: Int): String = {
-    (num % 3, num % 5) match {
-      case (0, 0) => "FizzBuzz"
-      case (0, _) => "Fizz"
-      case (_, 0) => "Buzz"
-      case _      => num.toString
-    }
+    type FB = PartialFunction[Int, String]
+    val fb: FB = { case i if i % 15 == 0 => "FizzBuzz" }
+    val b: FB = { case i if i % 5 == 0 => "Fizz" }
+    val f: FB = { case i if i % 3 == 0 => "Buzz" }
+    val a: FB = { case i => i.toString }
+    val fizzBuzz = fb orElse f orElse b orElse a
+    fizzBuzz(num)
   }
+
 }
 
